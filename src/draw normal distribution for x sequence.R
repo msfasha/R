2721905c247a498a3e81b988be934dbd2829bfb5
Mohammad Fasha <mohd.fasha@gmail.com>
2023-05-05 08:45:11 +0300

    Modify drawing normal dist by using by=.01

--- src/draw normal distribution for x sequence.R
+++ src/draw normal distribution for x sequence.R
@@ -5,6 +5,9 @@ pnorm(0)
 
 
 x <- seq(from = -4, to = 4, length.out = 200)
+# or
+# x <- seq(from = -4, to = 4, by = 0.1)
+
 plot(x, dnorm(x), type = "l",
      ylab = "Density", lwd = 4, col = 1, ylim = c(0, 0.6))
 
